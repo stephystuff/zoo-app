b8168a3c4d1c462d77d8177b7f382575c82857be
StephyStuff <stephanie.birch13@gmail.com>
2016-11-15 16:31:13 -0500

    Now able to make new PolarBear and new ArcticFox.

--- src/js/zoo-app.js
+++ src/js/zoo-app.js
@@ -1,16 +1,32 @@
 (function() {
   'use strict';
 
-  function Animal(name, dob) {
-    this.name = name || 'Random';
-    this.dob = dob;
-  }
 
-// Animal();
-console.log(new Animal('sandy', 10));
-  // function Polarbear(name, color, birth) {
-  //   console.log(this, arguments);
-  // }
-  //
-  // var sandy = new Polarbear ('sandy', 'white', true,)
+    function PolarBear(name, dob, color, fur){
+      this.name = name || 'Random';
+      this.dob = dob || 'January 1, 2016';
+      this.color = color || 'brown';
+      if(typeof(fur) === 'boolean') {
+        this.fur = fur;
+      } else {
+        this.fur = false;
+      }
+      console.log(this, arguments);
+    }
+
+    function ArcticFox(name, dob, color, fur){
+      this.name = name || 'Miscellaneous';
+      this.dob = dob || 'January 1, 2014';
+      this.color = color || 'grey';
+      if(typeof(fur) === 'boolean') {
+        this.fur = fur;
+      } else {
+        this.fur = false;
+      }
+      console.log(this, arguments);
+    }
+
+    var sandy = new PolarBear('sandy', '1047618000000', 'white', true);
+    var tails = new ArcticFox('tails', '1170738000000', 'white', true);
+
 }());
