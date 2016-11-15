774666e8c08d75d68af5679395845835e2ba74d3
StephyStuff <stephanie.birch13@gmail.com>
2016-11-15 14:21:59 -0500

    Now able to make a new animal with constructor function.

--- src/js/zoo-app.js
+++ src/js/zoo-app.js
@@ -1,6 +1,16 @@
 (function() {
   'use strict';
 
-  console.log('In here!');
+  function Animal(name, dob) {
+    this.name = name || 'Random';
+    this.dob = dob;
+  }
 
+// Animal();
+console.log(new Animal('sandy', 10));
+  // function Polarbear(name, color, birth) {
+  //   console.log(this, arguments);
+  // }
+  //
+  // var sandy = new Polarbear ('sandy', 'white', true,)
 }());
