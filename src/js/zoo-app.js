df693fc98218aba6e7bd988c3e631ba22aa1a3d6
StephyStuff <stephanie.birch13@gmail.com>
2016-11-15 19:52:50 -0500

    Changed ArticFox to BlueJay. Able to create a new Blue Jay. Able to create an Animal. PolarBear now has Animal prototype. Switched back PolarBear's constructor. Mother PolarBear able to give birth to a new polarbear.

--- src/js/zoo-app.js
+++ src/js/zoo-app.js
@@ -1,32 +1,55 @@
 (function() {
   'use strict';
 
-
-    function PolarBear(name, dob, color, fur){
-      this.name = name || 'Random';
-      this.dob = dob || 'January 1, 2016';
-      this.color = color || 'brown';
-      if(typeof(fur) === 'boolean') {
-        this.fur = fur;
-      } else {
-        this.fur = false;
-      }
-      console.log(this, arguments);
-    }
-
-    function ArcticFox(name, dob, color, fur){
+    function Animal(name, dob) {
+      console.log('inside animal', this);
       this.name = name || 'Miscellaneous';
-      this.dob = dob || 'January 1, 2014';
-      this.color = color || 'grey';
-      if(typeof(fur) === 'boolean') {
-        this.fur = fur;
-      } else {
-        this.fur = false;
-      }
-      console.log(this, arguments);
+      this.dob = dob || 'September 10, 2010';
     }
 
-    var sandy = new PolarBear('sandy', '1047618000000', 'white', true);
-    var tails = new ArcticFox('tails', '1170738000000', 'white', true);
+    // Constructor function to create a new Polar Bear
+    function PolarBear(name, dob, color, mammal){
+      Animal.apply(this, [name, dob]);
+      this.dob = dob || 10;
+      this.color = color || 'brown';
+      if(typeof(mammal) === 'boolean') {
+        this.mammal = mammal;
+      } else {
+        this.mammal = false;
+      }
+      // console.log(this, arguments);
+    }
 
+    PolarBear.prototype = Object.create(Animal.prototype);
+    PolarBear.prototype.constructor = PolarBear;
+
+    var motherPolarBear = new PolarBear('Rudy', 30, 'white', true);
+
+    PolarBear.prototype.giveBirth = function giveBirth() {
+      return new PolarBear('Timmy', 1, 'white', true);
+    };
+    console.log(motherPolarBear.giveBirth());
+
+    // Constructor function to create a new Turtle
+    function BlueJay(name, dob, color, mammal){
+      Animal.apply(this, [name, dob]);
+      this.dob = dob || 10;
+      this.color = color || 'grey';
+      if(typeof(mammal) === 'boolean') {
+        this.mammal = mammal;
+      } else {
+        this.mammal = false;
+      }
+      // console.log(this, arguments);
+    }
+
+    BlueJay.prototype = Object.create(Animal.prototype);
+    BlueJay.prototype.constructor = BlueJay;
+
+    var sandy = new PolarBear('Sandy', 26, 'white', true);
+    var jacob = new BlueJay('Jacob', 5, 'green', false);
+
+    // console.log('sandy:', sandy);
+
+    console.log(sandy.mammal);
 }());
