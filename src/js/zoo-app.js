a0eae89e14b9cf5301f0e8c7557f4b52cf381ac6
StephyStuff <stephanie.birch13@gmail.com>
2016-11-15 21:01:50 -0500

    Created motherBlueJay. Now able lay an egg and create new blue jay. Added swim method for polar bear, and fly method for bluejay.

--- src/js/zoo-app.js
+++ src/js/zoo-app.js
@@ -1,6 +1,7 @@
 (function() {
   'use strict';
 
+    // Constructor functioni to create an animal
     function Animal(name, dob) {
       console.log('inside animal', this);
       this.name = name || 'Miscellaneous';
@@ -25,13 +26,19 @@
 
     var motherPolarBear = new PolarBear('Rudy', 30, 'white', true);
 
+    // Function for motherPolarBear to give birth to a new Polar Bear
     PolarBear.prototype.giveBirth = function giveBirth() {
       return new PolarBear('Timmy', 1, 'white', true);
     };
     console.log(motherPolarBear.giveBirth());
 
+    // Species-specific method for duration of swimtime
+    PolarBear.prototype.swim = function swim(duration) {
+      return ('8 minutes');
+    };
+
     // Constructor function to create a new Turtle
-    function BlueJay(name, dob, color, mammal){
+    function BlueJay(name, dob, color, mammal, reptile){
       Animal.apply(this, [name, dob]);
       this.dob = dob || 10;
       this.color = color || 'grey';
@@ -40,14 +47,32 @@
       } else {
         this.mammal = false;
       }
+      if(typeof(reptile) === 'boolean') {
+        this.reptile = reptile;
+      } else {
+        this.reptile = false;
+      }
       // console.log(this, arguments);
     }
 
     BlueJay.prototype = Object.create(Animal.prototype);
     BlueJay.prototype.constructor = BlueJay;
 
-    var sandy = new PolarBear('Sandy', 26, 'white', true);
-    var jacob = new BlueJay('Jacob', 5, 'green', false);
+    var motherBlueJay = new BlueJay('Janet', 10, 'blue', false, true);
+
+    // Function for motherBlueJay to lay an egg and create a new Blue Jay
+    BlueJay.prototype.layEgg = function layEgg(number){
+      return new BlueJay('Janie', 1, 'blue', false, true);
+    };
+    console.log(motherBlueJay.layEgg(1));
+
+    // Species-specific method for duratioin of flying
+    BlueJay.prototype.fly = function fly(duration) {
+      return ('20 minutes');
+    };
+
+    var sandy = new PolarBear('Sandy', 26, 'white', true, false);
+    var jacob = new BlueJay('Jacob', 5, 'blue', false, true);
 
     // console.log('sandy:', sandy);
 
